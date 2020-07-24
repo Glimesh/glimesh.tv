15afc754ae0c898bcfa7e7633403055fbe23340a
mja00 <me@mja00.dev>
2020-07-24 14:55:10 -0400

    Added convert migration
    Creates displayname column, copies username over and then converts the username column to lowercase. 
    Reversing this copies displayname back into username, sets displayname to null and then deletes it.

--- priv/repo/migrations/20200724162310_add_displayname_to_user_auth_table.exs
+++ priv/repo/migrations/20200724162310_add_displayname_to_user_auth_table.exs
@@ -5,5 +5,15 @@ defmodule Glimesh.Repo.Migrations.AddDisplaynameToUserAuthTable do
     alter table(:users) do
       add :displayname, :string, default: nil
     end
+    execute "UPDATE users SET displayname = username"
+    execute "UPDATE users SET username = lower(username)"
+  end
+
+  def down do
+    execute "UPDATE users SET username = displayname"
+    execute "UPDATE users SET displayname = NULL"
+    alter table(:users) do
+      remove :displayname
+    end
   end
 end
