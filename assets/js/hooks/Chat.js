11567d8faf55eff05dbb1b6a33cf7b53e1129b56
clone1018 <luke@axxim.net>
2020-08-13 03:18:19 -0400

    Inject the emoji whereever the cursor is

--- assets/js/hooks/Chat.js
+++ assets/js/hooks/Chat.js
@@ -6,13 +6,13 @@ export default {
         const picker = new EmojiButton({
             theme: 'dark',
             position: 'top-start',
-            style: 'twemoji',
+            autoHide: false,
         });
-        const trigger = document.querySelector('.feather-message-square');
+        const trigger = document.querySelector('.emoji-activator');
         const chat = document.getElementById('chat_message_message');
-
         picker.on('emoji', selection => {
-            chat.value += selection.emoji;
+            const [start, end] = [chat.selectionStart, chat.selectionEnd];
+            chat.value = chat.value.substring(0, start) + selection.emoji + chat.value.substring(end, chat.value.length);
         });
 
         trigger.addEventListener('click', () => picker.togglePicker(trigger));
