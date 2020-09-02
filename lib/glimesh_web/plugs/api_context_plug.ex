87b705b7347a8eedf030a53c2bb49e6c38b2f5eb
WildWolf <magnus1997.mho@gmail.com>
2020-09-02 07:30:03 +0200

    Api now accepts Oauth2 Auth header as authorization

--- lib/glimesh_web/plugs/api_context_plug.ex
+++ lib/glimesh_web/plugs/api_context_plug.ex
@@ -1,13 +1,17 @@
 defmodule GlimeshWeb.Plugs.ApiContextPlug do
   @behaviour Plug
 
+  alias ExOauth2Provider.{
+    Keys,
+    Plug.ErrorHandler
+  }
   import Plug.Conn
   import Phoenix.Controller, only: [json: 2]
 
   def init(opts), do: opts
 
-  def call(conn, _) do
-    if authorized(conn) do
+  def call(conn, opts) do
+    if authorized(conn, opts) do
       context = build_context(conn)
       Absinthe.Plug.put_options(conn, context: context)
     else
@@ -35,7 +39,66 @@ defmodule GlimeshWeb.Plugs.ApiContextPlug do
     end
   end
 
-  def authorized(conn) do
-    conn.assigns[:current_user]
+  def authorized(conn, opts) do
+    if get_req_header(conn, "authorization") != [] do
+      key    = Keyword.get(opts, :key, :default)
+      config = [otp_app: :glimesh]
+
+      conn
+      |> fetch_token(opts)
+      |> verify_token(conn, key, config)
+      |> get_current_access_token(key)
+      |> handle_authentication(conn, key)
+      |> IO.inspect()
+    else
+      conn.assigns[:current_user]
+    end
+  end
+
+  defp fetch_token(conn, opts) do
+    ["Bearer " <> token] = get_req_header(conn, "authorization")
+
+    token
+  end
+
+  defp do_fetch_token(_realm_regex, []), do: nil
+  defp do_fetch_token(nil, [token | _tail]), do: String.trim(token)
+  defp do_fetch_token(realm_regex, [token | tail]) do
+    trimmed_token = String.trim(token)
+
+    case Regex.run(realm_regex, trimmed_token) do
+      [_, match] -> String.trim(match)
+      _          -> do_fetch_token(realm_regex, tail)
+    end
+  end
+
+  defp verify_token(nil, conn, _, _config), do: conn
+  defp verify_token("", conn, _, _config), do: conn
+  defp verify_token(token, conn, key, config) do
+    access_token = ExOauth2Provider.authenticate_token(token, config)
+
+    set_current_access_token(conn, access_token, key)
+  end
+
+  defp set_current_access_token(conn, access_token, the_key) do
+    put_private(conn, Keys.access_token_key(the_key), access_token)
+  end
+
+  defp handle_authentication({:ok, _}, conn, _opts), do: conn
+  defp handle_authentication({:error, reason}, %{params: params} = conn, opts) do
+    params = Map.put(params, :reason, reason)
+
+    conn
+    |> assign(:ex_oauth2_provider_failure, reason)
+    |> halt()
+    |> ErrorHandler.unauthenticated(params)
+  end
+
+  defp get_current_access_token(conn, the_key) do
+    case conn.private[Keys.access_token_key(the_key)] do
+      {:ok, access_token} -> {:ok, access_token}
+      {:error, error}     -> {:error, error}
+      _                   -> {:error, :no_session}
+    end
   end
 end
