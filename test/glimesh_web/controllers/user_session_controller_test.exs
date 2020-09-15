1dad3d6b364567c8281d5993bb1d141ff612afb6
mja00 <teamnirvana2@gmail.com>
2020-09-14 22:40:28 -0400

    Code quality makes it pretty again

--- test/glimesh_web/controllers/user_session_controller_test.exs
+++ test/glimesh_web/controllers/user_session_controller_test.exs
@@ -69,12 +69,18 @@ defmodule GlimeshWeb.UserSessionControllerTest do
     test "emits error message with banned user", %{conn: conn, banned_user: banned_user} do
       conn =
         post(conn, Routes.user_session_path(conn, :create), %{
-          "user" => %{"email" => banned_user.email, "password" => valid_user_password(), "tfa" => nil}
+          "user" => %{
+            "email" => banned_user.email,
+            "password" => valid_user_password(),
+            "tfa" => nil
+          }
         })
 
       response = html_response(conn, 200)
       assert response =~ "<h3>Login to our Alpha!</h3>"
-      assert response =~ "User account is banned. Please contact support at support@glimesh.tv for more information."
+
+      assert response =~
+               "User account is banned. Please contact support at support@glimesh.tv for more information."
     end
   end
 
