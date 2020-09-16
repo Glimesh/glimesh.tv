87c0e4bad8b6c272d19e9fc572e0c030a4954cd3
Matt Artist <11252574+mja00@users.noreply.github.com>
2020-09-16 07:24:14 -0400

    Added Korean to the list of languages we support! (#154)
    * Extracted gettext strings from master 87f99ce
    
    * Added Korean to the list of languages we support!
    
    * Fixed scope strings

--- config/config.exs
+++ config/config.exs
@@ -33,7 +33,7 @@ config :glimesh, GlimeshWeb.Emails.Mailer,
 
 config :glimesh, GlimeshWeb.Gettext,
   default_locale: "en",
-  locales: ~w(en es ja de nb es_MX es_AR fr sv vi ru)
+  locales: ~w(en es ja de nb es_MX es_AR fr sv vi ru ko)
 
 config :ex_oauth2_provider, namespace: Glimesh
 config :ex_oauth2_provider, ExOauth2Provider,
