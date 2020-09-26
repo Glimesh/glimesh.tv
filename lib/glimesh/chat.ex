e8c4591a337a1ab2c0ad77527dcf0cd7229ae11d
mja00 <teamnirvana2@gmail.com>
2020-09-26 15:59:50 -0400

    Made the create_chat_message func less complex

--- lib/glimesh/chat.ex
+++ lib/glimesh/chat.ex
@@ -101,24 +101,7 @@ defmodule Glimesh.Chat do
       raise ArgumentError, message: "User must not be banned"
     end
 
-    # Need to add this since phoenix likes strings and our tests don't use them :)
-    message_contain_link_helper =
-      if attrs["message"] do
-        message_contains_link(attrs["message"])
-      else
-        if attrs.message, do: message_contains_link(attrs.message), else: [true]
-      end
-
-    create_message =
-      case message_contain_link_helper do
-        [true] ->
-          if channel.block_links, do: false, else: true
-
-        _ ->
-          true
-      end
-
-    if create_message do
+    if allow_link_in_message(channel, attrs) do
       %ChatMessage{
         channel: channel,
         user: user
@@ -225,6 +208,25 @@ defmodule Glimesh.Chat do
       [{^username, _}] -> false
       [] -> true
     end
+  end
+
+  def allow_link_in_message(channel, attrs) do
+    # Need to add this since phoenix likes strings and our tests don't use them :)
+    message_contain_link_helper =
+      if attrs["message"] do
+        message_contains_link(attrs["message"])
+      else
+        if attrs.message, do: message_contains_link(attrs.message), else: [true]
+      end
+
+    create_message =
+      case message_contain_link_helper do
+        [true] ->
+          if channel.block_links, do: false, else: true
+
+        _ ->
+          true
+      end
   end
 
   def render_global_badge(user) do
