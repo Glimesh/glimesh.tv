1bc538a01f389ecd6d1a5ebd6846e0487239e135
mja00 <teamnirvana2@gmail.com>
2020-10-14 11:29:42 -0400

    Added invalid user page and check

--- lib/glimesh_web/controllers/gct_controller.ex
+++ lib/glimesh_web/controllers/gct_controller.ex
@@ -13,29 +13,38 @@ defmodule GlimeshWeb.GctController do
   def username_lookup(conn, params) do
     user = Accounts.get_by_username(params["query"], true)
 
-    render(
-      conn,
-      "lookup_user.html",
-      user: user,
-      payout_history: Payments.list_payout_history(user),
-      payment_history: Payments.list_payment_history(user)
-    )
+    if user do
+      render(
+        conn,
+        "lookup_user.html",
+        user: user,
+        payout_history: Payments.list_payout_history(user),
+        payment_history: Payments.list_payment_history(user)
+      )
+    else
+      render(conn, "invalid_user.html")
+    end
+
   end
 
   def edit_user_profile(conn, %{"username" => username}) do
-    user = Accounts.get_by_username(username, true)
-    user_changeset = Accounts.change_user_profile(user)
-
     unless CommunityTeam.can_edit_user_profile(conn.assigns.current_user) do
       redirect(conn, to: Routes.gct_path(conn, :username_lookup, query: username))
     end
 
-    render(
-      conn,
-      "edit_user_profile.html",
-      user: user,
-      user_changeset: user_changeset
-    )
+    user = Accounts.get_by_username(username, true)
+
+    if user do
+      user_changeset = Accounts.change_user_profile(user)
+      render(
+        conn,
+        "edit_user_profile.html",
+        user: user,
+        user_changeset: user_changeset
+      )
+    else
+      render(conn, "invalid_user.html")
+    end
   end
 
   def update_user_profile(conn, %{"user" => user_params, "username" => username}) do
@@ -55,19 +64,23 @@ defmodule GlimeshWeb.GctController do
   end
 
   def edit_user(conn, %{"username" => username}) do
-    user = Accounts.get_by_username(username, true)
-    user_changeset = Accounts.change_user(user)
-
     unless CommunityTeam.can_edit_user(conn.assigns.current_user) do
       redirect(conn, to: Routes.gct_path(conn, :username_lookup, query: username))
     end
 
-    render(
-      conn,
-      "edit_user.html",
-      user: user,
-      user_changeset: user_changeset
-    )
+    user = Accounts.get_by_username(username, true)
+
+    if user do
+      user_changeset = Accounts.change_user(user)
+      render(
+        conn,
+        "edit_user.html",
+        user: user,
+        user_changeset: user_changeset
+      )
+    else
+      render(conn, "invalid_user.html")
+    end
   end
 
   def update_user(conn, %{"user" => user_params, "username" => username}) do
