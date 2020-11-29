763235ec8e2581e0782f062a9cce5c1989a514c4
Luke Strickland <luke@axxim.net>
2020-11-28 19:27:27 -0600

    Allow env vars to be omitted if not used (#276)

--- config/releases.exs
+++ config/releases.exs
@@ -15,7 +15,7 @@ database_url =
 config :glimesh, Glimesh.Repo,
   # ssl: true,
   url: database_url,
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+  pool_size: String.to_integer(System.get_env("POOL_SIZE", "10"))
 
 # Endpoint Configuration
 secret_key_base =
@@ -29,14 +29,12 @@ live_view_signing_salt =
   System.get_env("LIVE_VIEW_SIGNING_SALT") ||
     raise """
     environment variable LIVE_VIEW_SIGNING_SALT is missing.
-    You can generate one by calling: mix phx.gen.secret
+    You can generate one by calling: mix phx.gen.secret 32
     """
 
-url_host = System.get_env("URL_HOST") || raise "environment variable URL_HOST is missing."
-url_port = System.get_env("URL_PORT") || raise "environment variable URL_PORT is missing."
-url_scheme = System.get_env("URL_SCHEME") || raise "environment variable URL_SCHEME is missing."
-http_port = System.get_env("HTTP_PORT") || raise "environment variable URL_PORT is missing."
-https_port = System.get_env("HTTPS_PORT") || raise "environment variable URL_PORT is missing."
+url_host = System.fetch_env!("URL_HOST")
+url_port = System.fetch_env!("URL_PORT")
+url_scheme = System.fetch_env!("URL_SCHEME")
 
 config :glimesh, GlimeshWeb.Endpoint,
   server: true,
@@ -50,23 +48,17 @@ config :glimesh, GlimeshWeb.Endpoint,
   secret_key_base: secret_key_base,
   live_view: [signing_salt: live_view_signing_salt]
 
-if http_port !== "" do
+if http_port = System.get_env("HTTP_PORT") do
   config :glimesh, GlimeshWeb.Endpoint,
     http: [
       port: http_port
     ]
 end
 
-if https_port !== "" do
-  https_key_file =
-    System.get_env("HTTPS_KEY_FILE") || raise "environment variable HTTPS_KEY_FILE is missing."
-
-  https_cert_file =
-    System.get_env("HTTPS_CERT_FILE") || raise "environment variable HTTPS_CERT_FILE is missing."
-
-  https_cacert_file =
-    System.get_env("HTTPS_CACERT_FILE") ||
-      raise "environment variable HTTPS_CACERT_FILE is missing."
+if https_port = System.get_env("HTTPS_PORT") do
+  https_key_file = System.fetch_env!("HTTPS_KEY_FILE")
+  https_cert_file = System.fetch_env!("HTTPS_CERT_FILE")
+  https_cacert_file = System.fetch_env!("HTTPS_CACERT_FILE")
 
   config :glimesh, GlimeshWeb.Endpoint,
     https: [
@@ -80,38 +72,20 @@ if https_port !== "" do
 end
 
 # Email Configuration
-mailgun_api_key =
-  System.get_env("MAILGUN_API_KEY") ||
-    raise """
-    environment variable MAILGUN_API_KEY is missing.
-    """
+if mailgun_api_key = System.get_env("MAILGUN_API_KEY") do
+  mailgun_domain = System.fetch_env!("MAILGUN_DOMAIN")
 
-mailgun_domain =
-  System.get_env("MAILGUN_DOMAIN") ||
-    raise """
-    environment variable MAILGUN_DOMAIN is missing.
-    """
-
-config :glimesh, GlimeshWeb.Emails.Mailer,
-  adapter: Bamboo.MailgunAdapter,
-  api_key: mailgun_api_key,
-  domain: mailgun_domain
+  config :glimesh, GlimeshWeb.Emails.Mailer,
+    adapter: Bamboo.MailgunAdapter,
+    api_key: mailgun_api_key,
+    domain: mailgun_domain
+end
 
 # Stripe Configuration
-stripe_public_api_key =
-  System.get_env("STRIPE_PUBLIC_API_KEY") ||
-    raise "environment variable STRIPE_PUBLIC_API_KEY is missing."
-
-stripe_api_key =
-  System.get_env("STRIPE_API_KEY") || raise "environment variable STRIPE_API_KEY is missing."
-
-stripe_connect_client_id =
-  System.get_env("STRIPE_CONNECT_CLIENT_ID") ||
-    raise "environment variable STRIPE_CONNECT_CLIENT_ID is missing."
-
-stripe_webhook_secret =
-  System.get_env("STRIPE_WEBHOOK_SECRET") ||
-    raise "environment variable STRIPE_WEBHOOK_SECRET is missing."
+stripe_public_api_key = System.fetch_env!("STRIPE_PUBLIC_API_KEY")
+stripe_api_key = System.fetch_env!("STRIPE_API_KEY")
+stripe_connect_client_id = System.fetch_env!("STRIPE_CONNECT_CLIENT_ID")
+stripe_webhook_secret = System.fetch_env!("STRIPE_WEBHOOK_SECRET")
 
 config :stripity_stripe,
   public_api_key: stripe_public_api_key,
@@ -120,57 +94,41 @@ config :stripity_stripe,
   webhook_secret: stripe_webhook_secret
 
 # hCaptcha Configuration
-hcaptcha_public_key =
-  System.get_env("HCAPTCHA_PUBLIC_KEY") ||
-    raise "environment variable HCAPTCHA_PUBLIC_KEY is missing."
-
-hcaptcha_secret =
-  System.get_env("HCAPTCHA_SECRET") ||
-    raise "environment variable HCAPTCHA_SECRET is missing."
-
-config :hcaptcha,
-  public_key: hcaptcha_public_key,
-  secret: hcaptcha_secret
+if hcaptcha_public_key = System.get_env("HCAPTCHA_PUBLIC_KEY") do
+  hcaptcha_secret = System.fetch_env!("HCAPTCHA_SECRET")
+  config :hcaptcha,
+    public_key: hcaptcha_public_key,
+    secret: hcaptcha_secret
+end
 
 # Waffle Configuration
-do_spaces_public_key =
-  System.get_env("DO_SPACES_PUBLIC_KEY") ||
-    raise "environment variable DO_SPACES_PUBLIC_KEY is missing."
+if System.get_env("WAFFLE_ENDPOINT") == "S3" do
+  do_spaces_public_key = System.fetch_env!("DO_SPACES_PUBLIC_KEY")
+  do_spaces_private_key = System.fetch_env!("DO_SPACES_PRIVATE_KEY")
+  do_spaces_bucket = System.fetch_env!("DO_SPACES_BUCKET")
+  waffle_asset_host = System.fetch_env!("WAFFLE_ASSET_HOST")
 
-do_spaces_private_key =
-  System.get_env("DO_SPACES_PRIVATE_KEY") ||
-    raise "environment variable DO_SPACES_PRIVATE_KEY is missing."
+  config :waffle,
+    storage: Waffle.Storage.S3,
+    bucket: do_spaces_bucket,
+    asset_host: waffle_asset_host
 
-do_spaces_bucket =
-  System.get_env("DO_SPACES_BUCKET") ||
-    raise "environment variable DO_SPACES_BUCKET is missing."
-
-waffle_asset_host =
-  System.get_env("WAFFLE_ASSET_HOST") ||
-    raise "environment variable WAFFLE_ASSET_HOST is missing."
-
-config :waffle,
-  storage: Waffle.Storage.S3,
-  bucket: do_spaces_bucket,
-  asset_host: waffle_asset_host
-
-config :ex_aws,
-  access_key_id: do_spaces_public_key,
-  secret_access_key: do_spaces_private_key,
-  region: "us-east-1",
-  s3: [
-    scheme: "https://",
-    host: "nyc3.digitaloceanspaces.com",
-    region: "us-east-1"
-  ]
+  config :ex_aws,
+    access_key_id: do_spaces_public_key,
+    secret_access_key: do_spaces_private_key,
+    region: "us-east-1",
+    s3: [
+      scheme: "https://",
+      host: "nyc3.digitaloceanspaces.com",
+      region: "us-east-1"
+    ]
+end
 
 # Glimesh Configuration
-email_physical_address =
-  System.get_env("GLIMESH_EMAIL_PHYSICAL_ADDRESS") ||
-    raise "environment variable GLIMESH_EMAIL_PHYSICAL_ADDRESS is missing."
-
-config :glimesh,
-  email_physical_address: email_physical_address
+if email_physical_address = System.get_env("GLIMESH_EMAIL_PHYSICAL_ADDRESS") do
+  config :glimesh,
+    email_physical_address: email_physical_address
+end
 
 # Default App Config
 config :glimesh, :stripe_config,
