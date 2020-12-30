fd7ada19d958573e3e5f75e64d3d1e8c1c895431
Luke Strickland <luke@axxim.net>
2020-12-30 15:35:51 -0500

    Removing unused test code

--- test/glimesh/streams/channel_notifier_test.exs
+++ test/glimesh/streams/channel_notifier_test.exs
@@ -79,28 +79,5 @@ defmodule Glimesh.Streams.ChannelNotifierTest do
 
       assert_no_emails_delivered()
     end
-
-    # test "update_channel/2 with valid data updates the channel" do
-    #   [channel, streamer] = channel_streamer_fixture()
-    #   assert channel.title == "Live Stream!"
-    #   assert channel.category_id == 1
-    #   {:ok, channel} = Streams.update_channel(streamer, channel, @valid_attrs)
-    #   assert channel.title == "Valid Title"
-    #   assert channel.category_id == 2
-    # end
-
-    # test "update_channel/2 with invalid data defaults to default values" do
-    #   [channel, streamer] = channel_streamer_fixture()
-    #   {:ok, channel} = Streams.update_channel(streamer, channel, @invalid_attrs)
-    #   assert channel.title == nil
-    # end
-
-    # test "update_channel/2 with blank chat rules defaults to nil" do
-    #   [channel, streamer] = channel_streamer_fixture()
-    #   {:ok, channel} = Streams.update_channel(streamer, channel, %{chat_rules_md: "Test rules"})
-    #   assert channel.chat_rules_html =~ "<p>\nTest rules</p>\n"
-    #   {:ok, channel} = Streams.update_channel(streamer, channel, @blank_chat_rules)
-    #   assert channel.chat_rules_html == nil
-    # end
   end
 end
