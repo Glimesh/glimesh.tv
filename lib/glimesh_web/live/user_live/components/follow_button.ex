18689c8925473f010d8a8e69d768c0ec9223af04
Drehd <69818373+Drehd@users.noreply.github.com>
2021-01-16 21:45:55 -0500

    tests

--- lib/glimesh_web/live/user_live/components/follow_button.ex
+++ lib/glimesh_web/live/user_live/components/follow_button.ex
@@ -9,7 +9,7 @@ defmodule GlimeshWeb.UserLive.Components.FollowButton do
     <%= if @user do %>
       <%= if @following do %>
         <div class="btn-group" role="group">
-          <button class="btn btn-primary btn-block follow-button" phx-click="unfollow"><i class="fas fa-user-minus"></i></button>
+          <button class="btn btn-primary btn-block follow-button" phx-click="unfollow"><span class="uf_txt"><%= gettext("Unfollow") %></span><span class="uf_ico"><i class="fas fa-user-minus"></i></span></button>
           <%= if @following.has_live_notifications do %>
           <button type="button" class="btn btn-primary live-notifications-button" phx-click="disable_live_notifications"><i class="fas fa-bell"></i></button>
           <% else %>
@@ -17,11 +17,11 @@ defmodule GlimeshWeb.UserLive.Components.FollowButton do
           <% end %>
         </div>
       <% else %>
-        <button class="btn btn-primary btn-block follow-button" phx-click="follow" phx-throttle="5000"><i class="fas fa-user-plus"></i></button>
+        <button class="btn btn-primary btn-block follow-button" phx-click="follow" phx-throttle="5000"><span class="f_txt"><%= gettext("Follow") %></span><span class="f_ico"><i class="fas fa-user-plus"></i></span></button>
       <% end %>
     <% else %>
       <%= link to: Routes.user_registration_path(@socket, :new), class: "btn btn-primary btn-block" do %>
-      <i class="fas fa-user-plus"></i>
+      <span class="f_txt"><%= gettext("Follow") %></span><span class="f_ico"><i class="fas fa-user-plus"></i></span>
       <% end %>
     <% end %>
     """
