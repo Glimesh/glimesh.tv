82119329c1c6d55749270a8b9553a4107c4c8d5e
Luke Strickland <luke@axxim.net>
2021-01-21 17:44:59 -0500

    Running mix format post mja's GCT changes

--- lib/glimesh_web/live/user_settings/components/profile_settings_live.ex
+++ lib/glimesh_web/live/user_settings/components/profile_settings_live.ex
@@ -6,12 +6,11 @@ defmodule GlimeshWeb.UserSettings.Components.ProfileSettingsLive do
     if session["locale"], do: Gettext.put_locale(session["locale"])
 
     {:ok,
-    socket
-    |> put_flash(:info, nil)
-    |> assign(:profile_changeset, session["profile_changeset"])
-    |> assign(:twitter_auth_url, session["twitter_auth_url"])
-    |> assign(:user, session["user"])
-    |> assign(:route, session["route"])}
+     socket
+     |> put_flash(:info, nil)
+     |> assign(:profile_changeset, session["profile_changeset"])
+     |> assign(:twitter_auth_url, session["twitter_auth_url"])
+     |> assign(:user, session["user"])
+     |> assign(:route, session["route"])}
   end
-
 end
