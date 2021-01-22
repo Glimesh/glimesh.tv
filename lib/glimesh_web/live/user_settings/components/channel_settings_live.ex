96c199b53d04ba2bcab82073d035dc40efcc4796
Luke Strickland <luke@axxim.net>
2021-01-22 10:24:10 -0500

    Running mix format post mja's GCT changes (#400)

--- lib/glimesh_web/live/user_settings/components/channel_settings_live.ex
+++ lib/glimesh_web/live/user_settings/components/channel_settings_live.ex
@@ -8,32 +8,37 @@ defmodule GlimeshWeb.UserSettings.Components.ChannelSettingsLive do
     if session["locale"], do: Gettext.put_locale(session["locale"])
 
     {:ok,
-    socket
-    |> put_flash(:info, nil)
-    |> put_flash(:error, nil)
-    |> assign(:channel_changeset, session["channel_changeset"])
-    |> assign(:categories, session["categories"])
-    |> assign(:channel, session["channel"])
-    |> assign(:route, session["route"])
-    |> assign(:user, session["user"])
-    |> assign(:delete_route, session["delete_route"])
-    |> assign(:channel_delete_disabled, session["channel_delete_disabled"])}
+     socket
+     |> put_flash(:info, nil)
+     |> put_flash(:error, nil)
+     |> assign(:channel_changeset, session["channel_changeset"])
+     |> assign(:categories, session["categories"])
+     |> assign(:channel, session["channel"])
+     |> assign(:route, session["route"])
+     |> assign(:user, session["user"])
+     |> assign(:delete_route, session["delete_route"])
+     |> assign(:channel_delete_disabled, session["channel_delete_disabled"])}
   end
 
   @impl true
   def handle_event("rotate_stream_key", _params, socket) do
-    with :ok <- Bodyguard.permit(Glimesh.Streams, :update_channel, socket.assigns.channel.user, socket.assigns.channel) do
+    with :ok <-
+           Bodyguard.permit(
+             Glimesh.Streams,
+             :update_channel,
+             socket.assigns.channel.user,
+             socket.assigns.channel
+           ) do
       case Streams.rotate_stream_key(socket.assigns.channel.user, socket.assigns.channel) do
         {:ok, changeset} ->
           {:noreply,
-          socket
-          |> put_flash(:info, "Stream key reset")
-          |> assign(:channel_changeset, Streams.Channel.changeset(changeset))}
+           socket
+           |> put_flash(:info, "Stream key reset")
+           |> assign(:channel_changeset, Streams.Channel.changeset(changeset))}
+
         {:error, _changeset} ->
-          {:noreply,
-          socket}
+          {:noreply, socket}
       end
     end
   end
-
 end
