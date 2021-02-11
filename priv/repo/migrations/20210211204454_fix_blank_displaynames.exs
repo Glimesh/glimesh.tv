c41e7f1226d1ca17054dd5e53089d24ed45769c8
mja00 <teamnirvana2@gmail.com>
2021-02-11 15:48:30 -0500

    Adding migration to update any blank displaynames

---
+++ priv/repo/migrations/20210211204454_fix_blank_displaynames.exs
@@ -0,0 +1,15 @@
+defmodule Glimesh.Repo.Migrations.FixBlankDisplaynames do
+  use Ecto.Migration
+
+  import Ecto.Query
+
+  def change do
+
+    Glimesh.Repo.update_all(
+      from(u in "users",
+      where: is_nil(u.displayname),
+      update: [set: [displayname: u.username]])
+    )
+
+  end
+end
