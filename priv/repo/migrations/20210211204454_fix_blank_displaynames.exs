710fc15b8d65954ca0a879f31c0f688863046255
mja00 <teamnirvana2@gmail.com>
2021-02-11 15:53:10 -0500

    Use the right function

--- priv/repo/migrations/20210211204454_fix_blank_displaynames.exs
+++ priv/repo/migrations/20210211204454_fix_blank_displaynames.exs
@@ -8,7 +8,9 @@ defmodule Glimesh.Repo.Migrations.FixBlankDisplaynames do
     Glimesh.Repo.update_all(
       from(u in "users",
       where: is_nil(u.displayname),
-      update: [set: [displayname: u.username]])
+      update: [set: [displayname: u.username]]
+      ),
+      []
     )
 
   end
