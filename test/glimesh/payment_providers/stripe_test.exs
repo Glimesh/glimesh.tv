7e2d4fce7fa7f757f6a7c8af379b8422eddf6445
Luke Strickland <luke@axxim.net>
2021-02-12 08:32:41 -0500

    Adding additional tests for stripe connection

--- test/glimesh/payment_providers/stripe_test.exs
+++ test/glimesh/payment_providers/stripe_test.exs
@@ -99,4 +99,134 @@ defmodule Glimesh.PaymentProviders.StripeProviderTest do
       end)
     end
   end
+
+  describe "Stripe Connecting" do
+    setup do
+      %{
+        streamer: streamer_fixture(),
+        user: user_fixture()
+      }
+    end
+
+    test "start_connect/4 returns a stripe url", %{streamer: streamer} do
+      assert {:ok, url} =
+               StripeProvider.start_connect(
+                 streamer,
+                 "US",
+                 "https://localhost/some-return",
+                 "https://localhost/some-refresh"
+               )
+
+      assert url =~ "https://"
+      assert url =~ "dev.stripe.me/setup"
+
+      another_streamer = streamer_fixture()
+
+      assert {:ok, url} =
+               StripeProvider.start_connect(
+                 another_streamer,
+                 "UK",
+                 "https://localhost/some-return",
+                 "https://localhost/some-refresh"
+               )
+
+      assert url =~ "https://"
+      assert url =~ "dev.stripe.me/setup"
+    end
+
+    test "start_connect/4 continues where you left off", %{streamer: streamer} do
+      assert {:ok, url} =
+               StripeProvider.start_connect(
+                 streamer,
+                 "US",
+                 "https://localhost/some-return",
+                 "https://localhost/some-refresh"
+               )
+
+      refreshed_streamer = Glimesh.Accounts.get_user!(streamer.id)
+      refute is_nil(refreshed_streamer.stripe_user_id)
+
+      assert {:ok, new_url} =
+               StripeProvider.start_connect(
+                 refreshed_streamer,
+                 "US",
+                 "https://localhost/some-return",
+                 "https://localhost/some-refresh"
+               )
+
+      assert url == new_url
+    end
+
+    test "start_connect/4 wont allow you to change country", %{streamer: streamer} do
+      assert {:ok, _} =
+               StripeProvider.start_connect(
+                 streamer,
+                 "US",
+                 "https://localhost/some-return",
+                 "https://localhost/some-refresh"
+               )
+
+      refreshed_streamer = Glimesh.Accounts.get_user!(streamer.id)
+
+      assert {:error,
+              "You are not allowed to setup Glimesh payouts for this country. Please contact support@glimesh.tv"} =
+               StripeProvider.start_connect(
+                 refreshed_streamer,
+                 "UK",
+                 "https://localhost/some-return",
+                 "https://localhost/some-refresh"
+               )
+    end
+
+    test "check_account_capabilities_and_upgrade/1 upgrades an account", %{streamer: streamer} do
+      account_mock = %Stripe.Account{
+        id: "1234",
+        capabilities: %{
+          transfers: "active"
+        },
+        tos_acceptance: %{
+          date: 1234
+        }
+      }
+
+      Glimesh.Accounts.set_stripe_user_id(streamer, "1234")
+
+      assert {:ok, user} = StripeProvider.check_account_capabilities_and_upgrade(account_mock)
+      refute is_nil(user.stripe_user_id)
+      assert user.can_receive_payments
+      assert Glimesh.Accounts.can_receive_payments?(user)
+    end
+
+    test "check_account_capabilities_and_upgrade/1 has error states" do
+      assert {:pending,
+              "Verifying that your account can receive transfers. Please check back later."} =
+               StripeProvider.check_account_capabilities_and_upgrade(%Stripe.Account{
+                 id: "1234",
+                 capabilities: %{
+                   transfers: "pending"
+                 },
+                 tos_acceptance: %{
+                   date: 1234
+                 }
+               })
+
+      assert {:pending, "Verifying your Terms of Service acceptance. Please check back later."} =
+               StripeProvider.check_account_capabilities_and_upgrade(%Stripe.Account{
+                 id: "1234",
+                 capabilities: %{
+                   transfers: "active"
+                 },
+                 tos_acceptance: %{
+                   date: 0
+                 }
+               })
+
+      assert {:pending, "Verifying your account details. Please check back later."} =
+               StripeProvider.check_account_capabilities_and_upgrade(%Stripe.Account{
+                 id: "1234",
+                 capabilities: %{},
+                 tos_acceptance: %{}
+               })
+    end
+  end
 end
