57cb096a510120920951514eadef03f74636732e
Luke Strickland <luke@axxim.net>
2021-02-13 09:12:44 -0500

    Adding logic for routing viewers to close edge locations (#446)
    * Adding logic for routing viewers to close edge locations
    
    * Adding error if database is empty

---
+++ lib/glimesh/janus.ex
@@ -0,0 +1,52 @@
+defmodule Glimesh.Janus do
+  @moduledoc """
+  Logic to help with Janus
+  """
+  import Ecto.Query, warn: false
+
+  alias Glimesh.Janus.EdgeRoute
+  alias Glimesh.Repo
+
+  @doc """
+  Fetches either the closest edge host depending on location, or falls back to a safe globally available alternative.
+  """
+  def get_closest_edge_location(country) when is_binary(country) do
+    if edge = one_edge_by_location(country) do
+      edge
+    else
+      one_good_edge()
+    end
+  end
+
+  def get_closest_edge_location(_) do
+    one_good_edge()
+  end
+
+  def all_edge_routes do
+    Repo.all(EdgeRoute)
+  end
+
+  def create_edge_route(attrs \\ %{}) do
+    %EdgeRoute{}
+    |> EdgeRoute.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  defp one_good_edge do
+    Repo.one(
+      from e in EdgeRoute,
+        where: e.available == true,
+        order_by: [desc: :priority],
+        limit: 1
+    )
+  end
+
+  defp one_edge_by_location(country) do
+    Repo.one(
+      from e in EdgeRoute,
+        where: e.available == true and ^country in e.country_codes,
+        order_by: [desc: :priority],
+        limit: 1
+    )
+  end
+end
--- test/glimesh_web/live/user_live/stream_test.exs
+++ test/glimesh_web/live/user_live/stream_test.exs
@@ -42,4 +42,46 @@ defmodule GlimeshWeb.UserLive.StreamTest do
       assert page =~ "<video"
     end
   end
+
+  describe "Edge Routing" do
+    setup do
+      Glimesh.Janus.create_edge_route(%{
+        hostname: "some-de-server",
+        url: "https://some-de-server/janus",
+        priority: 1.0,
+        available: true,
+        country_codes: ["DE", "AT"]
+      })
+
+      Glimesh.Janus.create_edge_route(%{
+        hostname: "some-us-server",
+        url: "https://some-us-server/janus",
+        priority: 1.1,
+        available: true,
+        country_codes: ["US", "MX"]
+      })
+
+      streamer = streamer_fixture()
+
+      %{
+        channel: streamer.channel,
+        streamer: streamer
+      }
+    end
+
+    test "gets sent to the correct edge location", %{conn: conn, streamer: streamer} do
+      conn =
+        conn
+        |> Plug.Conn.put_req_header(
+          "cf-ipcountry",
+          "DE"
+        )
+
+      {:ok, view, _} = live(conn, Routes.user_stream_path(conn, :index, streamer.username))
+
+      send(view.pid, {:load_stream, "DE"})
+      assert render(view) =~ "some-de-server"
+      assert render(view) =~ "https://some-de-server/janus"
+    end
+  end
 end
