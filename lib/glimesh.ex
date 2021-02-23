0b9d3dccdc7284d143f90637e38aa5f00c5d3ca3
Luke Strickland <luke@axxim.net>
2021-02-23 10:13:21 -0500

    Launching payouts

--- lib/glimesh.ex
+++ lib/glimesh.ex
@@ -10,4 +10,8 @@ defmodule Glimesh do
   def get_version do
     Keyword.get(Application.spec(:glimesh), :vsn, "unknown")
   end
+
+  def has_launched? do
+    NaiveDateTime.diff(~N[2021-03-02 16:00:00], NaiveDateTime.utc_now(), :millisecond) < 0
+  end
 end
