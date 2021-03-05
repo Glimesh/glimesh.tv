621f4eafbe63c5f8d62003270687516aecfcbb6c
Luke Strickland <luke@axxim.net>
2021-03-05 13:41:26 -0500

    Autoplay video & audio

--- assets/js/hooks/FtlVideo.js
+++ assets/js/hooks/FtlVideo.js
@@ -7,24 +7,39 @@ let player;
 export default {
     mounted() {
         let container = this.el;
-        
+
         this.handleEvent("load_video", ({janus_url, channel_id}) => {
             player = new FtlPlayer(container, janus_url);
-            let init = player.init(channel_id);
-        })
+            console.debug(`load_video event for janus_url=${janus_url} channel_id=${channel_id}`)
+            player.init(channel_id);
+        });
 
+        let lastVolume = localStorage.getItem("player-volume");
+        if (lastVolume) {
+            container.volume = parseFloat(lastVolume);
+        }
+
+        container.addEventListener("volumechange", (event) => {
+            if (container.volume) {
+                localStorage.setItem("player-volume", container.volume);
+            }
+        });
+     
         container.addEventListener("loadeddata", function() {
             let playPromise = container.play();
             if (playPromise !== undefined) {
                 playPromise.then(_ => {
                   // Autoplay started!
                 }).catch(error => {
-                    alert("Video autoplay was prevented by your browser, hit the Play button!")
+                    container.muted = true;
+                    container.play();
                 });
               }
         })
     },
     destroyed() {
-        if(player) {player.destroy();}
+        if(player) {
+            player.destroy();
+        }
     }
 };
