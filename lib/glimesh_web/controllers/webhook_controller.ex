212bdc2c43b3c0a2c93dab4f2ccf17906de503ac
Luke Strickland <luke@axxim.net>
2021-03-12 10:42:34 -0500

    Handling Stripe webhook call (#565)

--- lib/glimesh_web/controllers/webhook_controller.ex
+++ lib/glimesh_web/controllers/webhook_controller.ex
@@ -2,11 +2,22 @@ defmodule GlimeshWeb.WebhookController do
   use GlimeshWeb, :controller
 
   def stripe(%Plug.Conn{assigns: %{stripe_event: stripe_event}} = conn, _params) do
-    Glimesh.PaymentProviders.StripeProvider.Webhooks.handle_webhook(stripe_event)
+    case Glimesh.PaymentProviders.StripeProvider.Webhooks.handle_webhook(stripe_event) do
+      {:ok, _} ->
+        conn
+        |> send_resp(:ok, "Accepted.")
+        |> halt()
 
-    conn
-    |> send_resp(:ok, "")
-    |> halt()
+      {:error, message} when is_binary(message) ->
+        conn
+        |> send_resp(:bad_request, message)
+        |> halt()
+
+      _ ->
+        conn
+        |> send_resp(:bad_request, "Unknown error")
+        |> halt()
+    end
   end
 
   def taxidpro(%Plug.Conn{assigns: %{taxidpro_body: taxidpro_body}} = conn, _params) do
