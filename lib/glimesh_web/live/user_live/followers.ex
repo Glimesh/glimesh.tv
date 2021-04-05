2dadd02c70d539944a505c6a432bde650b19b791
Luke Strickland <luke@axxim.net>
2021-04-05 08:43:24 -0400

    Adding full search to your followers page, showing all following users (#600)

--- lib/glimesh_web/live/user_live/followers.ex
+++ lib/glimesh_web/live/user_live/followers.ex
@@ -8,15 +8,25 @@ defmodule GlimeshWeb.UserLive.Followers do
 
     case Accounts.get_by_username(username) do
       %Glimesh.Accounts.User{} = streamer ->
-        followers = Glimesh.AccountFollows.list_followers(streamer)
-
         {:ok,
          socket
          |> assign(:streamer, streamer)
-         |> assign(:users, followers)}
+         |> handle_page()}
 
       nil ->
         {:ok, redirect(socket, to: "/")}
     end
+  end
+
+  def handle_page(%{assigns: %{live_action: :followers, streamer: streamer}} = socket) do
+    socket
+    |> assign(:page_title, gettext("%{username}'s Followers", username: streamer.displayname))
+    |> assign(:users, Glimesh.AccountFollows.list_followers(streamer))
+  end
+
+  def handle_page(%{assigns: %{live_action: :following, streamer: streamer}} = socket) do
+    socket
+    |> assign(:page_title, gettext("%{username}'s Following", username: streamer.displayname))
+    |> assign(:users, Glimesh.AccountFollows.list_following(streamer))
   end
 end
