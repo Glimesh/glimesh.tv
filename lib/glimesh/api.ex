10fc0ce7232002b2034fa7814d889a37ebd9eef0
Luke Strickland <luke@axxim.net>
2021-04-28 07:59:13 -0400

    Add chat message validation (#662)
    * Add chat message validation
    
    * Satisfying code quality requirements

--- lib/glimesh/api.ex
+++ lib/glimesh/api.ex
@@ -8,4 +8,15 @@ defmodule Glimesh.Api do
   def error_not_found, do: {:error, @error_not_found}
 
   def error_access_denied, do: {:error, @error_access_denied}
+
+  @doc """
+  Parse a list of changset errors into actionable API errors
+  """
+  def parse_ecto_changeset_errors(%Ecto.Changeset{} = changeset) do
+    Ecto.Changeset.traverse_errors(changeset, fn {msg, opts} ->
+      Enum.reduce(opts, msg, fn {key, value}, acc ->
+        String.replace(acc, "%{#{key}}", to_string(value))
+      end)
+    end)
+  end
 end
