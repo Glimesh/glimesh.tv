c753b8a6bd999f5f3f40263e9484f29598ebfd6c
Luke Strickland <luke@axxim.net>
2021-05-23 07:29:00 -0400

    Working on some more tests

--- test/glimesh_web/live/gct_live/manage_emotes_test.exs
+++ test/glimesh_web/live/gct_live/manage_emotes_test.exs
@@ -1,27 +1,33 @@
 defmodule GlimeshWeb.GctLive.ManageEmotesTest do
   use GlimeshWeb.ConnCase
 
-  import Glimesh.AccountsFixtures
   import Phoenix.LiveViewTest
 
+  @glimchef %{
+    last_modified: 1_594_171_879_000,
+    name: "glimchef.svg",
+    content: File.read!("test/assets/glimchef.svg"),
+    size: 19_056,
+    type: "image/svg+xml"
+  }
+
   describe "Emotes Management" do
-    # test "lists some users", %{conn: conn} do
-    #   user = user_fixture()
-
-    #   {:ok, _, html} = live(conn, Routes.user_index_path(conn, :index))
-
-    #   assert html =~ "Our Users"
-    #   assert html =~ user.displayname
-    # end
+    setup [:register_and_log_in_gct_user]
 
     test "can upload emotes", %{conn: conn} do
-      # Ensures the user we test for is not on the first page
-      Enum.each(1..20, fn _ -> user_fixture() end)
-      user = user_fixture()
+      {:ok, view, _} = live(conn, Routes.gct_manage_emotes_path(conn, :index))
 
-      {:ok, view, _} = live(conn, Routes.user_index_path(conn, :index))
+      avatar =
+        file_input(view, "#emote_upload", :emote, [
+          @glimchef
+        ])
 
-      assert render_submit(view, :search, %{q: user.username}) =~ user.displayname
+      IO.inspect(avatar)
+
+      assert render_upload(avatar, "glimchef.svg") =~ "glimchef"
+
+      assert view |> form("#emote_upload", )
+      assert render_submit(view, %{"0" => "glimchef"}) =~ "Successfully uploaded emotes"
     end
   end
 end
