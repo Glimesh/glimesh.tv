31d24570135023e7d5c0d661ab8f0bb8af4edef3
Luke Strickland <luke@axxim.net>
2021-06-01 11:55:26 -0400

    Fix int parsing

--- config/releases.exs
+++ config/releases.exs
@@ -207,7 +207,8 @@ if show_staging_warning = System.get_env("GLIMESH_SHOW_STAGING_WARNING") do
 end
 
 if max_emotes = System.get_env("GLIMESH_MAX_CHANNEL_EMOTES") do
-  config :glimesh, Glimesh.Emotes, max_channel_emotes: max_emotes
+  {max, _} = Integer.parse(max_emotes)
+  config :glimesh, Glimesh.Emotes, max_channel_emotes: max
 end
 
 if allow_animated_emotes = System.get_env("GLIMESH_ALLOW_CHANNEL_ANIMATED_EMOTES") do
