907b96f996a6486d4fc80dfda45725a2b44b4643
Ember <68715225+EmberShadow@users.noreply.github.com>
2021-06-04 13:54:31 +0100

    Alphabetical order Pt 2

--- lib/glimesh_web/live/user_settings/components/channel_statistics_live.ex
+++ lib/glimesh_web/live/user_settings/components/channel_statistics_live.ex
@@ -1,26 +1,18 @@
 defmodule GlimeshWeb.UserSettings.Components.ChannelStatisticsLive do
   	use GlimeshWeb, :live_view
-
-  	alias Glimesh.ChannelStatistics
+	
   	alias Glimesh.Accounts
-  	alias Glimesh.ChannelLookups
-
+	alias Glimesh.ChannelLookups
+  	alias Glimesh.ChannelStatistics
 
   	def mount(_params, session, socket) do
-
-
     	streamer = Accounts.get_user_by_session_token(session["user_token"])
-
 		case ChannelLookups.get_channel_for_user(streamer) do
   			%Glimesh.Streams.Channel{} = channel ->
-
 				{:ok, 
 				socket
-
 				|> assign(:channel, channel)
-
 			}  	
-
   			nil ->
   				
 			    {:ok, redirect(socket, to: "/")}
