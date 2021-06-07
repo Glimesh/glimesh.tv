7d3240d8f4eeeee76867f41406b4b420f7242dfd
EmberShadow <jadegaunt1998@gmail.com>
2021-06-08 00:00:24 +0100

    Remove extra crap

--- lib/glimesh_web/live/user_settings/components/channel_statistics_live.ex
+++ lib/glimesh_web/live/user_settings/components/channel_statistics_live.ex
@@ -17,24 +17,4 @@ defmodule GlimeshWeb.UserSettings.Components.ChannelStatisticsLive do
         {:ok, redirect(socket, to: "/")}
     end
   end
-
-  use GlimeshWeb, :live_view
-
-  alias Glimesh.Accounts
-  alias Glimesh.ChannelLookups
-  alias Glimesh.ChannelStatistics
-
-  def mount(_params, session, socket) do
-    streamer = Accounts.get_user_by_session_token(session["user_token"])
-
-    case ChannelLookups.get_channel_for_user(streamer) do
-      %Glimesh.Streams.Channel{} = channel ->
-        {:ok,
-         socket
-         |> assign(:channel, channel)}
-
-      nil ->
-        {:ok, redirect(socket, to: "/")}
-    end
-  end
 end
