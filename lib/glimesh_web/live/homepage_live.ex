317e17eec5f8f617d1deb6c7f7ed0379fbd3908b
Luke Strickland <luke@axxim.net>
2021-06-17 14:41:58 -0500

    Adding a temporary homepage for GCX (#715)

--- lib/glimesh_web/live/homepage_live.ex
+++ lib/glimesh_web/live/homepage_live.ex
@@ -9,7 +9,11 @@ defmodule GlimeshWeb.HomepageLive do
     if session["locale"], do: Gettext.put_locale(session["locale"])
 
     channels = get_cached_channels()
+
     random_channel = get_random_channel(channels)
+    # Temporarily making GCX the default homepage stream when they are live
+    random_channel = get_gcx_or_random(random_channel)
+
     user_count = Glimesh.Accounts.count_users()
 
     {:ok,
@@ -20,6 +24,30 @@ defmodule GlimeshWeb.HomepageLive do
      |> assign(:random_channel_thumbnail, get_stream_thumbnail(random_channel))
      |> assign(:user_count, user_count)
      |> assign(:current_user, maybe_user)}
+  end
+
+  defp get_gcx_or_random(random_channel) do
+    now = NaiveDateTime.utc_now()
+
+    maybe_gcx_channel = Glimesh.ChannelLookups.search_live_channels(%{"ids" => ["15710"]})
+
+    # Slightly larger slots just in case
+    gcx_slots = [
+      [~N[2021-06-17 15:00:00], ~N[2021-06-17 21:00:00]],
+      [~N[2021-06-18 15:00:00], ~N[2021-06-18 21:00:00]],
+      [~N[2021-06-19 15:00:00], ~N[2021-06-19 21:00:00]]
+    ]
+
+    gcx_live? =
+      Enum.any?(gcx_slots, fn [start_time, end_time] ->
+        now > start_time and now < end_time
+      end)
+
+    if length(maybe_gcx_channel) == 1 and gcx_live? and hd(maybe_gcx_channel).status == "live" do
+      hd(maybe_gcx_channel)
+    else
+      random_channel
+    end
   end
 
   defp get_stream_thumbnail(%Glimesh.Streams.Channel{} = channel) do
