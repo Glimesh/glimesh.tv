b54f4e75f7c6cd4abd4bbee069561109f1dd5987
Luke Strickland <luke@axxim.net>
2021-07-08 10:23:45 -0400

    Removing commented code

--- test/glimesh_web/live/support_modal/support_modal_test.exs
+++ test/glimesh_web/live/support_modal/support_modal_test.exs
@@ -74,40 +74,4 @@ defmodule GlimeshWeb.SupportModalTest do
 
     Glimesh.Accounts.get_user!(streamer.id)
   end
-
-  describe "Authenticated User Support Modal" do
-    setup [:register_and_log_in_streamer]
-
-    # test "can set streamloots url", %{conn: conn, user: user, channel: channel} do
-    #   {:ok, view, _html} =
-    #     live_isolated(conn, GlimeshWeb.ChannelSettingsLive.Addons, session: %{"user" => user})
-
-    #   form = view |> element("#addons")
-
-    #   assert render_submit(form, %{
-    #            "channel" => %{"streamloots_url" => "https://www.streamloots.com/ferret"}
-    #          }) =~
-    #            "Updated addons."
-
-    #   assert Glimesh.ChannelLookups.get_channel!(channel.id).streamloots_url ==
-    #            "https://www.streamloots.com/ferret"
-    # end
-
-    # test "can clear streamloots url", %{conn: conn, user: user, channel: channel} do
-    #   {:ok, _} =
-    #     Glimesh.Streams.update_addons(user, channel, %{
-    #       "streamloots_url" => "https://www.streamloots.com/ferret"
-    #     })
-
-    #   {:ok, view, _html} =
-    #     live_isolated(conn, GlimeshWeb.ChannelSettingsLive.Addons, session: %{"user" => user})
-
-    #   form = view |> element("#addons")
-
-    #   assert render_submit(form, %{"channel" => %{"streamloots_url" => ""}}) =~
-    #            "Updated addons."
-
-    #   assert is_nil(Glimesh.ChannelLookups.get_channel!(channel.id).streamloots_url)
-    # end
-  end
 end
