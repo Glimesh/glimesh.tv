8a6b1f7e8be98a51ffa1f30724f8fb284446202f
mja00 <teamnirvana2@gmail.com>
2021-08-16 14:47:59 -0400

    Increasing coverage

---
+++ test/glimesh_web/live/gct_live/button_array_test.exs
@@ -0,0 +1,26 @@
+defmodule GlimeshWeb.GctLive.ButtonArrayTest do
+  use GlimeshWeb.ConnCase
+
+  import Phoenix.LiveViewTest
+  import Glimesh.AccountsFixtures
+
+
+  describe "Button array on user lookup" do
+    setup [:register_and_log_in_gct_user]
+
+    test "can remove 2fa", %{conn: conn, user: user} do
+      lookup_user = user_fixture(%{tfa_token: "Wow look at this token"})
+      {:ok, view, html} = live_isolated(conn, GlimeshWeb.GctLive.Components.ButtonArray, session: %{"admin" => user, "user" => lookup_user})
+      assert html =~ "Remove 2FA"
+
+      {:ok, conn} =
+        view
+        |> element("a", "Remove 2FA")
+        |> render_click()
+        |> follow_redirect(conn)
+
+      refute conn.resp_body =~ "Remove 2FA"
+    end
+
+  end
+end
