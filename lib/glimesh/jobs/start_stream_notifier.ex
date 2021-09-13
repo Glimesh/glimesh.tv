61a799131d7d9da23f2a41934496de825becc239
Luke Strickland <luke@axxim.net>
2021-09-13 09:51:57 -0400

    Don't attempt to send an email if the stream is not live (#752)

--- lib/glimesh/jobs/start_stream_notifier.ex
+++ lib/glimesh/jobs/start_stream_notifier.ex
@@ -5,11 +5,15 @@ defmodule Glimesh.Jobs.StartStreamNotifier do
   def perform([channel_id]) do
     channel = Glimesh.ChannelLookups.get_channel!(channel_id)
     users = Glimesh.ChannelLookups.list_live_subscribed_followers(channel)
+    full_channel = Glimesh.Repo.preload(channel, [:user, :stream, :tags])
 
-    Glimesh.Streams.ChannelNotifier.deliver_live_channel_notifications(
-      users,
-      Glimesh.Repo.preload(channel, [:user, :stream, :tags])
-    )
+    # There's a chance the stream has already gone offline before the email is triggered.
+    if Glimesh.Streams.is_live?(full_channel) do
+      Glimesh.Streams.ChannelNotifier.deliver_live_channel_notifications(
+        users,
+        full_channel
+      )
+    end
 
     :ok
   end
