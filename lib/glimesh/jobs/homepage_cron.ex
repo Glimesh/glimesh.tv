1837ad2e898d9225b9b5da94a21858304eb45b1e
Luke Strickland <luke@axxim.net>
2021-12-06 10:29:21 -0500

    Adding exception catching on the jobs

--- lib/glimesh/jobs/homepage_cron.ex
+++ lib/glimesh/jobs/homepage_cron.ex
@@ -14,5 +14,20 @@ defmodule Glimesh.Jobs.HomepageCron do
     Rihanna.schedule(Glimesh.Jobs.HomepageCron, [], in: @interval)
 
     :ok
+  rescue
+    e ->
+      {:error, e}
+  end
+
+  def retry_at(_failure_reason, _args, attempts) when attempts < 10 do
+    seconds = attempts * 5
+    due_at = DateTime.add(DateTime.utc_now(), seconds, :second)
+    Logger.info("HomepageCron failed, retrying in #{seconds}")
+    {:ok, due_at}
+  end
+
+  def retry_at(_failure_reason, _args, _attempts) do
+    Logger.error("HomepageCron failed after 10 attempts")
+    :noop
   end
 end
