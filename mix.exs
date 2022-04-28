07446dbbcaa85313e2fcea966c7c45daaa614145
Luke Strickland <luke@axxim.net>
2022-04-28 15:20:36 -0400

    Forgotten files

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Glimesh.MixProject do
     [
       app: :glimesh,
       version: "0.1.0",
-      elixir: "~> 1.11",
+      elixir: "~> 1.13.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
