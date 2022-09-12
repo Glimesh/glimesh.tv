0ba6565b1cb962c0e0c5470271f9245dc4a2d680
Luke Strickland <luke@axxim.net>
2022-09-12 13:42:42 -0400

    Fixing mass failing tests

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,4 +1,5 @@
 {:ok, _} = Application.ensure_all_started(:ex_machina)
 ExUnit.start()
 Faker.start()
-Ecto.Adapters.SQL.Sandbox.mode(Glimesh.Repo, :manual)
+# This was causing issues with shared runtime for multiple users of the ecto sandbox
+# Ecto.Adapters.SQL.Sandbox.mode(Glimesh.Repo, :manual)
