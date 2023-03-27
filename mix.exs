1ccc053e5bb9ab3f5f404138d60fca81cf623b92
Luke Strickland <luke@axxim.net>
2023-03-27 10:38:04 -0400

    Fixing surface compiler

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Glimesh.MixProject do
       version: "0.1.0",
       elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: Mix.compilers(),
+      compilers: Mix.compilers() ++ [:surface],
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps(),
